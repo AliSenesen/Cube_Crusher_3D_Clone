c7ceb78ad4aaed0ef42ce03c37c4e9480b62a118
Burak <60696929+Burak-san@users.noreply.github.com>
2022-10-25 16:53:11 +0300

    initial

---
+++ Assets/Scripts/Signals/SaveLoadSignals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Data.ValueObject;
+using Extentions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class SaveLoadSignals : MonoSingleton<SaveLoadSignals>
+    {
+        public UnityAction<int> onSaveLevelId = delegate {  };
+        public UnityAction<int> onSaveLeft = delegate {  };
+        public UnityAction<MoneyData> onTotalMoney = delegate {  };
+        
+        public Func<int> onLoadLevelId;
+        public Func<int> onLoadLeft;
+        public Func<MoneyData> onLoadMoney;
+
+    }
+}
